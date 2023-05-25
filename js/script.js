a6375740b47285e9428fc9e838879b62b142464b
“N3M351” <gmtgeovane@gmail.com>
2023-05-25 01:13:34 -0300

    Performance and animations

--- js/script.js
+++ js/script.js
@@ -1,54 +1,72 @@
 const buttonDropdown = document.querySelector(".button_header");
 const menuDropdown = document.querySelector(".menu_header");
-buttonDropdown.addEventListener("click", ()=> menuDropdown.classList.toggle("active"));
 
-const cursoButton = [...document.querySelector(".bloco_button").children];
-const cursos = [...document.querySelector(".container_qualificacoes").children];
+buttonDropdown.addEventListener("click", () => {
+  menuDropdown.classList.toggle("active");
+});
+
+const menuLinks = Array.from(document.querySelectorAll(".menu_header a"));
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuDropdown.classList.remove("active");
+  });
+});
+
+const cursoButton = Array.from(document.querySelector(".bloco_button").children);
+const cursos = Array.from(document.querySelector(".container_qualificacoes").children);
 const educacao = document.querySelector(".educacao");
 
-function esconderCursos () {
-    cursos.forEach(curso => {curso.style.display = "none"});
-    cursoButton.forEach(button => {button.classList.remove("ativo")});
+function hideCursos() {
+  cursos.forEach((curso) => {
+    curso.style.display = "none";
+  });
+  cursoButton.forEach((button) => {
+    button.classList.remove("ativo");
+  });
 }
 
-function cursoTarget (id) {
-    const cursoCurrent = document.querySelector("#" + id);
-    cursoCurrent.style.display = "block"
+function showCurso(id) {
+  const cursoCurrent = document.querySelector("#" + id);
+  cursoCurrent.style.display = "block";
 }
 
-function selecionarCurso () {
-    cursoButton.forEach (button => {
-        button.addEventListener("click", (target)=> {
-            esconderCursos();
-            const cursoAtual = target.currentTarget;
-            cursoTarget(cursoAtual.dataset.id);
-            cursoAtual.className += " ativo"
-        })
-    })
+function selectCurso() {
+  cursoButton.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      hideCursos();
+      const cursoAtual = event.currentTarget;
+      showCurso(cursoAtual.dataset.id);
+      cursoAtual.classList.add("ativo");
+    });
+  });
 }
 
-function execute () {
-    esconderCursos();
-    selecionarCurso()
-    educacao.click()
+function execute() {
+  hideCursos();
+  selectCurso();
+  educacao.click();
 }
 
-window.addEventListener("load", execute())
+window.addEventListener("load", execute);
 
 const botaoCopiar = document.querySelectorAll(".botaoCopiar");
-botaoCopiar.forEach(item => {
-    item.addEventListener('click', ()=> {
-        if(navigator.clipboard.writeText(item.value)) {
-            item.id = "email-copiado_check"
-            item.textContent = "Email copiado";
-        }
-    
-        setInterval(()=> {
-            item.id= "email-copiado_check";
-            item.textContent = "Copiar email"
-        }, 3000);
-    })
-})
+botaoCopiar.forEach((item) => {
+  item.addEventListener("click", () => {
+    navigator.clipboard.writeText(item.value)
+      .then(() => {
+        item.id = "email-copiado_check";
+        item.textContent = "Email copiado";
+      })
+      .catch((error) => {
+        console.error("Failed to copy email:", error);
+      });
+
+    setTimeout(() => {
+      item.id = "";
+      item.textContent = "Copiar email";
+    }, 3000);
+  });
+});
 
 /*____________________________menu open________________________________________________________________*/
 
@@ -58,20 +76,27 @@ document.addEventListener("DOMContentLoaded", function () {
   var botaoTopo = document.querySelector(".botao_topo");
 
   var prevScrollPos = window.pageYOffset;
+  var isScrollingUp = false;
 
   function toggleHeader() {
     var currentScrollPos = window.pageYOffset;
 
     if (prevScrollPos > currentScrollPos) {
-      header.classList.add("show-header");
-      header.classList.remove("hide-header");
-      botaoTopo.style.opacity = "1";
-      botaoTopo.style.transform = "translateY(-3.2rem) translateX(-50%)";
+      if (!isScrollingUp) {
+        header.classList.add("show-header");
+        header.classList.remove("hide-header");
+        botaoTopo.style.opacity = "1";
+        botaoTopo.style.transform = "translateY(-3.2rem) translateX(-50%)";
+        isScrollingUp = true;
+      }
     } else if (currentScrollPos > 0) {
-      header.classList.add("hide-header");
-      header.classList.remove("show-header");
-      botaoTopo.style.opacity = "0";
-      botaoTopo.style.height = "0";
+      if (isScrollingUp) {
+        header.classList.add("hide-header");
+        header.classList.remove("show-header");
+        botaoTopo.style.opacity = "0";
+        botaoTopo.style.height = "0";
+        isScrollingUp = false;
+      }
     }
 
     prevScrollPos = currentScrollPos;
@@ -84,10 +109,12 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", handleScroll);
 });
 
+
 /*______________________page scroll button_____________________________*/
 
 document.addEventListener("DOMContentLoaded", function () {
   var footer1 = document.getElementById("bar");
+  var footer2 = document.getElementById("other-footer");
   var content = document.querySelector("main");
 
   function toggleFooter(footer) {
@@ -96,10 +123,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var windowHeight = window.innerHeight;
     var footerHeight = footer.offsetHeight;
 
-    if (yOffset === 0) {
-      footer.classList.add("show-footer");
-      footer.classList.remove("hide-footer");
-    } else if (yOffset + windowHeight >= contentHeight - footerHeight) {
+    if (yOffset === 0 || yOffset + windowHeight >= contentHeight - footerHeight) {
       footer.classList.add("show-footer");
       footer.classList.remove("hide-footer");
     } else {
@@ -113,8 +137,13 @@ document.addEventListener("DOMContentLoaded", function () {
     toggleFooter(footer2);
   }
 
+  function handleResize() {
+    toggleFooter(footer1);
+    toggleFooter(footer2);
+  }
+
   window.addEventListener("scroll", handleScroll);
-  window.addEventListener("resize", handleScroll);
+  window.addEventListener("resize", handleResize);
 });
 
 /*____________________bars hidden_______________________________*/
@@ -138,41 +167,57 @@ document.addEventListener("DOMContentLoaded", function () {
     toggleBotaoTopo();
   }
 
+  function handleResize() {
+    toggleBotaoTopo();
+  }
+
   window.addEventListener("scroll", handleScroll);
-  window.addEventListener("resize", handleScroll);
+  window.addEventListener("resize", handleResize);
 });
+
 
 /*_________________________________________________________________________*/
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', debounce(function() {
   var parallax = document.querySelector('.section_mainbg');
   var scrollPosition = window.pageYOffset;
   parallax.style.backgroundPositionY = scrollPosition * 0.7 + 'px';
   parallax.style.transform = 'translateY(' + scrollPosition * -0.3 + 'px)';
-});
+}, 10));
+
+// Debounce function to optimize performance
+function debounce(func, delay) {
+  var timeout;
+  return function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, delay);
+  };
+}
+
 
 
 /*_________________________________________________________________________*/
 
-// JavaScript to hide the loading screen when content is ready
-window.addEventListener('load', function () {
+window.addEventListener('load', function() {
   const loadingScreen = document.querySelector('.loading-screen');
-  const content = document.querySelector('.content');
+  const minimumTime = 1800; // 2 seconds
 
   // Show the loading screen initially
   loadingScreen.style.display = 'flex';
-  content.style.display = 'none';
-
-  // Set a minimum time in milliseconds
-  const minimumTime = 1200; // 2 seconds
 
   // Calculate the remaining time to reach the minimum time
   const loadTime = Date.now() - window.performance.timing.navigationStart;
   const remainingTime = Math.max(0, minimumTime - loadTime);
 
   // Delay hiding the loading screen until the minimum time has passed
-  setTimeout(function () {
-    loadingScreen.style.display = 'none';
-    content.style.display = 'none';
+  setTimeout(function() {
+    loadingScreen.style.opacity = '0';
+
+    // Set a new timeout to change the display property after a transition
+    setTimeout(function() {
+      loadingScreen.style.display = 'none';
+    }, 300); // 0.3 seconds (adjust as needed)
   }, remainingTime);
 });
+
+
